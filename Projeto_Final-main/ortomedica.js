06f061769aac053237f378592d9faae275ce7313
Gabriel Riguetto <gabrielriguetto3@gmail.com>
2025-02-20 08:33:25 -0300

    update botoes e paleta de cores do site

--- Projeto_Final-main/ortomedica.js
+++ Projeto_Final-main/ortomedica.js
@@ -34,3 +34,4 @@ window.onscroll = () => {
     searchForm.classList.remove('active');      // Fecha a busca ao scrollar
     cartItem.classList.remove('active');        // Fecha o carrinho ao scrollar
 }
+
